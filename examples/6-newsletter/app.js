6c2fae361525f2b7b6f0799472ddce817d3886f2
Ran You <ran.you8311@gmail.com>
2021-02-11 18:20:39 -0800

    newsletter complete

--- examples/6-newsletter/app.js
+++ examples/6-newsletter/app.js
@@ -2,3 +2,21 @@ const form = document.querySelector('.form')
 const emailInput = document.querySelector('.email-input')
 const alert = document.querySelector('.alert')
 alert.style.display = 'none'
+
+form.addEventListener('submit', async function (e){
+    e.preventDefault();
+    form.classList.add('loading')
+    alert.style.display='none'
+    const email=emailInput.value
+    try{
+        await axios.post('/api/6-newsletter',{email})
+    form.innerHTML=`<h4>Success! Please check your email</h4>`
+
+    } catch (error){
+        console.log(error.response)
+        alert.style.display='block'
+        alert.textContent=error.response.data.email[0]
+    }
+    form.classList.remove('loading')
+
+})
